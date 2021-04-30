61849748dd1d49ce7726e1f0afb8e42c84f594e6
Miguel Ibarra <mibar0005@gmail.com>
2021-04-30 11:22:00 -0700

    updated capstone

--- movies/src/app.js
+++ movies/src/app.js
@@ -1,11 +1,13 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const moviesRouter = require("./movies/movies.router");
 const theatersRouter = require("./theaters/theaters.router");
 const reviewsRouter = require("./reviews/reviews.router.js");
 
 app.use(express.json());
+app.use(cors());
 
 app.use("/movies", moviesRouter);
 app.use("/theaters", theatersRouter);
@@ -23,4 +25,4 @@ app.use((error, req, res, next) => {
   res.status(status).json({ error: message });
 });
 
-module.exports = app;
+module.exports = app; 
